55f0ce5ca03c5d8ab8d9836aaeb6c97dea948393
Mugo <keithgety@gmail.com>
2022-07-26 16:45:59 +0300

    refactor OrderLineItem interface name

--- src/main/java/backend/duka_kuu/repos/OrderLineItemsRepository.java
+++ src/main/java/backend/duka_kuu/repos/OrderLineItemsRepository.java
@@ -1,8 +1,8 @@
 package backend.duka_kuu.repos;
 
-import backend.duka_kuu.domain.OrderLineItems;
+import backend.duka_kuu.domain.OrderLineItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface OrderLineItemsRepository extends JpaRepository<OrderLineItems, Long> {
+public interface OrderLineItemsRepository extends JpaRepository<OrderLineItem, Long> {
 }
